cea5aba4e16e7d04a3a883e3bbbf637950fc7e9f
siddhesh128 <siddheshj039@gmail.com>
2025-05-28 00:18:53 +0530

    updated title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Electric transport Vehicle using pantograph mechanism',
+  description: 'Created by Siddhesh',
+  generator: 'Siddhesh',
 }
 
 export default function RootLayout({
